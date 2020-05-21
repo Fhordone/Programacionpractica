f9cc0f362cd76b9fa5452ee7e3a8030940d355ab
Fhordone <61848549+Fhordone@users.noreply.github.com>
2020-05-21 01:30:38 -0500

    Programacionpractica
    Actividad en casa de la clase del día sábado 16/05/2020
    Se trabajaron formularios, métodos y una pequeña suma en el formato CSHTML
    La tarea fue calcular el sueldo anual en una pestaña llamada EMPLEADO

---
+++ Actividad 02/tarea/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using tarea.Models;
+
+namespace tarea.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Empleado()
+        {
+            return View();
+        }
+
+        public IActionResult EmpleadoCreate(Empleado objempleado)
+        {
+            objempleado.Mensaje="Su sueldo anual es " + objempleado.sueldo * 12;
+            return View("Empleado",objempleado);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Actividad 02/tarea/Models/Empleado.cs
@@ -0,0 +1,11 @@
+using System;
+namespace tarea.Models
+{
+    public class Empleado {
+        public double sueldo {get; set;}
+
+        public double sueldoanual {get; set;}
+
+        public string Mensaje {get; set;}
+    }
+}
